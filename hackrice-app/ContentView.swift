b7dc998a764529ed8f4ddae62736253862b2a90d
Aidan Terlizzi <act5@rice.edu>
2022-09-23 22:07:29 -0500

    Initial Commit

---
+++ hackrice-app/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  hackrice-app
+//
+//  Created by Aidan Terlizzi on 9/23/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ hackrice-app/hackrice_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  hackrice_appApp.swift
+//  hackrice-app
+//
+//  Created by Aidan Terlizzi on 9/23/22.
+//
+
+import SwiftUI
+
+@main
+struct hackrice_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
