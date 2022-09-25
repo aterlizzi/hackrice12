8f06649ac8be63eddd69d28e015a5e172388235e
Aidan Terlizzi <act5@rice.edu>
2022-09-25 02:17:07 -0500

    final changes

--- server/controllers/door.controller.js
+++ server/controllers/door.controller.js
@@ -1,7 +1,8 @@
 import { openNearestDoor } from "../services/door.service.js";
 
 const openDoor = async (req, res, next) => {
-  const response = await openNearestDoor(req.body.coordinates);
+  const response = await openNearestDoor(req.body.coordinates, req.body.email);
+  console.log(response);
   res.json(response);
 };
 
